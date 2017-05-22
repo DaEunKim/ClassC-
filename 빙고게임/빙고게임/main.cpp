48579194a36704bf47d766886fb80e50c6fea468
DaEun Kim <ekdms717@kookmin.ac.kr>
2017-05-22 09:54:41 +0900

    빙고게임

--- 빙고게임/빙고게임/main.cpp
+++ 빙고게임/빙고게임/main.cpp
@@ -12,6 +12,74 @@
 #include <string>
 using namespace std;
 
+int board[5][5];
+bool done[5][5];
+int X, Y;
+
+bool checkCross(){
+    bool flag = 1;
+    for(int i=0; i<5; ++i)
+        if(!done[i][i])
+            flag = 0;
+    
+    if(flag)
+        return 1;
+    
+    flag = 1;
+    for(int i=0; i<5; ++i)
+        if(!done[i][4-i])
+            flag = 0;
+    
+    if(flag)
+        return 1;
+    
+    return 0;
+}
+
+bool checkPlus(int x, int y){
+    bool flag = 1;
+    for(int i=0; i<5; ++i)
+        if(!done[i][x])
+            flag = 0;
+    
+    if(flag)
+        return 1;
+    
+    flag = 1;
+    for(int i=0; i<5; ++i)
+        if(!done[y][i])
+            flag = 0;
+    
+    if(flag)
+        return 1;
+    
+    return 0;
+}
+
+bool checkPoint(){
+    if(done[0][0] && done[0][4] && done[4][0] && done[4][4])
+        return 1;
+    return 0;
+}
+
+bool isDone(int x, int y){
+    done[y][x] = 1;
+    
+    if(checkCross() || checkPlus(x, y) || checkPoint())
+        return 1;
+    return 0;
+}
+
+int searchPoint(int num){
+    for(int i=0; i<5; ++i)
+        for(int j=0; j<5; ++j)
+            if(board[i][j] == num){
+                X = j, Y = i;
+                return 1;
+            }
+    return 0;
+}
+
 int main(void){
     ifstream inStream;
     inStream.open("/Users/Dani/Documents/ClassCpp/빙고게임/빙고게임/input.txt");
@@ -23,6 +91,53 @@ int main(void){
     int T;
     inStream >> T;
     
+    while(T--){
+        for(int i=0; i<5; ++i)
+            for(int j=0; j<5; ++j)
+                inStream >> board[i][j], done[i][j] = 0;
+        
+        done[2][2] = 1;
+        
+        int callNum[75];
+        for(int i=0; i<75; ++i)
+            inStream >> callNum[i];
+        
+        int count = 1;
+        for(int i=0; i<75; ++i){
+            if(searchPoint(callNum[i])){
+                if(isDone(X, Y)){
+                    cout << count << "\n";
+                    break;
+                }
+            }
+            
+            count++;
+        }
+    }
+}
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /*
     for(int z = 0; z < T; z++){
         int arr[5][5]={0,};
         int save[5][5] = {0,};
@@ -67,7 +182,7 @@ int main(void){
             
             
         }
-        for(
+     */
 /*
         for(int i = 0;i<5;i++){
             for(int j = 0;j<5;j++){
@@ -87,11 +202,8 @@ int main(void){
         
   */
         
-        
-        
-        
-    }
     
+/*
     inStream.close();
 }
-
+*/
